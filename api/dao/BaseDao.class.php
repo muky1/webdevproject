146a4924375ee3823933a4787f346d36ccfb1ab6
muky1 <muhamed.begic@stu.ibu.edu.ba>
2021-03-21 15:04:55 +0100

    BaseDao

---
+++ api/dao/BaseDao.class.php
@@ -0,0 +1,44 @@
+<?php
+
+class BaseDao {
+  public function __construct(){
+    <?php
+$servername = "localhost";
+$username = "root";
+$password = "begic123";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=webproject", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
+
+
+  }
+
+  public function insert(){
+
+
+  }
+
+  public function update(){
+
+
+  }
+
+  public function query(){
+
+
+  }
+
+  public function query_unique(){
+
+
+  }
+
+
+}
+ ?>
