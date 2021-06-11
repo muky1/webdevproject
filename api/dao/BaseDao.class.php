4c603b7fb617e0874cd56d1bd018784621cc82b1
muky1 <muhamed.begic@stu.ibu.edu.ba>
2021-06-11 06:41:46 +0200

    added comments

--- api/dao/BaseDao.class.php
+++ api/dao/BaseDao.class.php
@@ -1,6 +1,13 @@
 <?php
 
 require_once dirname(__FILE__)."/../config.php";
+/**
+*The main class for interaction with database
+*
+*All other DAO classes should inherit this class
+*
+* @author Muhamed Begic
+*/
 
 class BaseDao {
 
